b2f056e3b27cbaac3b732e2dde70ecfc209c396e
Alejo calandra <alejocalandra18@gmail.com>
2023-06-06 21:22:36 -0300

    practicando condicionales

---
+++ clase-3-condicionales-bucles/js/condicionales.js
@@ -0,0 +1,68 @@
+// const edad = 10;
+
+// if (edad < 18) {
+// 	console.log('Es menor de edad');
+// } else {
+// 	console.log('es mayor de edad');
+// }
+
+// const nota = 1;
+
+// if (nota == 10) {
+// 	console.log('excelente');
+// } else if (nota == 8) {
+// 	console.log('Muy Bien');
+// } else if (nota == 6) {
+// 	console.log('bien');
+// } else if (nota <= 5) {
+// 	console.log('desaprobado');
+// }
+
+// const nombreUsuario = 'rolling';
+// const edad = 20;
+// const contraseña = 'admin123';
+// const confirmarContraseña = 'admin1234';
+
+// console.log(contraseña.length);
+
+// if (
+// 	nombreUsuario === '' ||
+// 	edad === '' ||
+// 	contraseña === '' ||
+// 	confirmarContraseña === ''
+// ) {
+// 	console.log('Todos los campos son obligatorios');
+// } else if (edad < 18) {
+// 	console.log('El usuario no cumple con las normas de la edad');
+// } else if (contraseña.length < 5) {
+// 	console.log('la contraseña debe ser mayor a 5 caracteres');
+// } else if (contraseña != confirmarContraseña) {
+// 	console.log('las contraseñas no son iguales');
+// } else {
+// 	console.log('usuario logueado');
+// }
+
+const estacion = prompt('ingrese una estacion climatica');
+
+if (estacion === 'verano') {
+	console.log('te gusta calor');
+} else if (estacion === 'invierno') {
+	console.log('te gusta el frio');
+} else if (estacion === 'otoño') {
+	console.log('te gusta los colores de otoño');
+} else if (estacion === 'primavera') {
+	console.log('te gusta ver florecer ');
+} else {
+	console.log('La estacion ingresada no es valida');
+}
+
+//Tarea
+// Tarea 3 - Números con condicionales
+// ● Ingresan dos números
+// ● Si el primero es mayor que el segundo mostrar en consola
+// ● la suma de ambos
+// ● Si el primero es menor que el segundo mostrar en consola
+// ● la resta del segundo menos el primero.
+// ● Si son iguales mostrar en consola la multiplicación de ambos.
+// ● ejemplo del mensaje: 2 es menor que 5, la resta de 5 - 2 es
+// igual a 3
